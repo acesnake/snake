01646504c555dfbd063aea3b2209569e65d446ce
acesnake <134964495+acesnake@users.noreply.github.com>
2023-05-29 13:32:07 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get references to the necessary HTML elements
+const gameBoard = document.getElementById('game-board');
+const scoreValue = document.getElementById('score-value');
+
+// Game logic goes here...
+// ...
+
+// Initial game update
+updateGame();
